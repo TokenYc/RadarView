1cb1a24425068dcc9f640011f03175d8d696e9af
tokenyc <247067345@qq.com>
2017-06-21 17:32:25 +0800

    完成雷达加载页面

--- app/src/main/java/net/archeryc/radarview/MainActivity.java
+++ app/src/main/java/net/archeryc/radarview/MainActivity.java
@@ -4,10 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final RadarView radarView = (RadarView) findViewById(R.id.radarView);
+        radarView.showLoading();
+        radarView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                radarView.stopLoading();
+            }
+        },3000);
     }
 }
--- app/src/main/java/net/archeryc/radarview/RadarView.java
+++ app/src/main/java/net/archeryc/radarview/RadarView.java
@@ -15,7 +15,7 @@ import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Interpolator;
 
 /**
- * 一句话功能简述
+ * 一句话功能简述 自定义雷达页面
  * 功能详细描述
  *
  * @author 杨晨 on 2017/6/16 13:57
@@ -35,7 +35,9 @@ public class RadarView extends View {
     private ObjectAnimator[] circleAnimators;
     private float[] animatorValues;
     private float[] circleRadius;
+    private float[] displayCircleRadius;
     private boolean isPlayAnimator=false;
+    private boolean shouldStopAnimator=false;
 
     public RadarView(Context context) {
         this(context,null);
@@ -107,24 +109,44 @@ public class RadarView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if (!isPlayAnimator()){
-            startAnimator(canvas);
-        }
-        for (int i=0;i<CIRCLE_NUM;i++){
-            canvas.drawCircle(getMeasuredWidth()/2,getMeasuredHeight()/2,animatorValues[i], circlePaints[i]);
+        if (animatorValues!=null&&circlePaints!=null) {
+            if (isPlayAnimator()) {
+                for (int i = 0; i < CIRCLE_NUM; i++) {
+                    canvas.drawCircle(getMeasuredWidth() / 2, getMeasuredHeight() / 2, animatorValues[i], circlePaints[i]);
+                }
+            }else{
+                for (int i = 0; i < CIRCLE_NUM; i++) {
+                    circlePaints[i].setAlpha(255);
+                    canvas.drawCircle(getMeasuredWidth() / 2, getMeasuredHeight() / 2, displayCircleRadius[i], circlePaints[i]);
+                }
+            }
         }
     }
 
-    private void startAnimator(final Canvas canvas) {
+
+    public void showLoading(){
+        post(new Runnable() {
+            @Override
+            public void run() {
+                startAnimator();
+            }
+        });
+    }
+    /**
+     * 显示正在加载状态
+     */
+    private void startAnimator() {
         setPlayAnimator(true);
         circleAnimators=new ObjectAnimator[CIRCLE_NUM];
         animatorValues = new float[CIRCLE_NUM];
         circleRadius = new float[CIRCLE_NUM];
+        displayCircleRadius = new float[CIRCLE_NUM];
 
         for (int i=0;i<CIRCLE_NUM;i++){
             circleRadius[i]=getMeasuredWidth()/2-i*getMeasuredWidth()/((CIRCLE_NUM)*2);
+            displayCircleRadius[i]=getMeasuredWidth()/2/5*(i+1);
             circleAnimators[i] = ObjectAnimator
-                    .ofFloat(canvas, "yc", circleRadius[i] / 3.5f, getMeasuredWidth()/(2+(float)i*i*i/4))
+                    .ofFloat(this, "yc", circleRadius[i] / 3.5f, getMeasuredWidth()/(2+(float)i*i*i/4))
                     .setDuration(1400);
             circleAnimators[i].setRepeatCount(ValueAnimator.INFINITE);
             circleAnimators[i].setInterpolator(new AccelerateInterpolator((float) (i+1)/CIRCLE_NUM));
@@ -143,7 +165,7 @@ public class RadarView extends View {
                     }else{
                         alpha= (int) (255*(1-percent)*2*(1-(float)finalI/4));
                     }
-                    Log.d("yc", "i===>"+finalI+"percent===>" + percent + "alpha====>" + alpha);
+//                    Log.d("yc", "i===>"+finalI+"percent===>" + percent + "alpha====>" + alpha);
                     circlePaints[finalI].setAlpha(alpha);
                     invalidate();
                 }
@@ -151,26 +173,47 @@ public class RadarView extends View {
             circleAnimators[i].addListener(new Animator.AnimatorListener() {
                 @Override
                 public void onAnimationStart(Animator animation) {
-
+                    Log.d("yc","onAnimationStart");
                 }
 
                 @Override
                 public void onAnimationEnd(Animator animation) {
-
+                    Log.d("yc","onAnimationEnd");
                 }
 
                 @Override
                 public void onAnimationCancel(Animator animation) {
-
+                    Log.d("yc","onAnimationCancel");
                 }
 
                 @Override
                 public void onAnimationRepeat(Animator animation) {
-                    //todo 判断是否终止了动画，保证一次动画可以完整走完
+                    Log.d("yc","onAnimationRepeat");
+                    //判断是否终止了动画，保证一次动画可以完整走完
+                    if (isShouldStopAnimator()){
+                        setShouldStopAnimator(false);
+                        setPlayAnimator(false);
+                        ObjectAnimator objectAnimator= (ObjectAnimator) animation;
+                        objectAnimator.removeAllUpdateListeners();
+                        objectAnimator.removeAllListeners();
+                        objectAnimator.cancel();
+                    }
                 }
             });
 
         }
+    }
+
+    public void stopLoading(){
+        setShouldStopAnimator(true);
+    }
+
+    public boolean isShouldStopAnimator() {
+        return shouldStopAnimator;
+    }
+
+    public void setShouldStopAnimator(boolean shouldStopAnimator) {
+        this.shouldStopAnimator = shouldStopAnimator;
     }
 
     public boolean isPlayAnimator() {
